c37b8e1c0b0254af1208c216d8283be9625605f0
Abhishek Kumar Singh <idforabhilaptop@gmail.com>
2025-09-21 15:28:19 +0530

    file added

--- Backend/ai_chat.js
+++ Backend/ai_chat.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const { GoogleGenAI } = require("@google/genai");      //googlegenai ek class hai
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyC_oyX8_hTOfBOzsO6ya09c8vMOLTQqGhM" });
+const ai = new GoogleGenAI({ apiKey: process.env.KEY });
 
 //frontend se jo message aaya hai vo pass kiya hai
 async function main(message) {
@@ -18,7 +19,7 @@ module.exports = main
 // const OpenAI = require('openai');
 // const openai = new OpenAI({
 //     baseURL: "https://openrouter.ai/api/v1",
-//     apiKey: "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",
+//     apiKey: process.env.KEY_2,
 // });
 // async function main(msg) {
 //     const completion = await openai.chat.completions.create({
